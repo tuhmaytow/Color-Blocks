9186b9554a218ec410fe698347317daecd697fe9
eunica lazatin <NicaLazatin@eunicas-MBP.attlocal.net>
2016-05-12 21:18:02 -0700

    Project 1

---
+++ Project 1.js
@@ -0,0 +1,17 @@
+//Project description:
+	A game very similar to Dr. Mario where the player has to match the color of the block to the element floating in the grid.
+
+
+//Who uses it?
+	Anybody who enjoys tetris-like games, and puzzles.
+
+//What outputs do they need?
+	Scoreboard, level, speed
+
+//What inputs are needed to generate those outputs?
+	spacebar and arrow keys
+
+//Technologies:
+	HTML, Javascript, CSS, Canvas, Bootstrap maybe?, Audio
+
+//Feature list:
