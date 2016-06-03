518446a0b73ac40d8a0116b77c73b9c29dc07711
eunica lazatin <NicaLazatin@eunicas-MacBook-Pro.local>
2016-06-02 17:48:00 -0700

    edited

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static("public"));
+const PORT = process.env["PORT"];
+const SERVER_NAME = "GAME";
 
-app.get("/index", function(req, res, next){
-	res.sendFile(__dirname + "/public/")
+app.use('/public', express.static("public"));
+
+app.get('/', function(req, res, next){
+	res.sendFile(__dirname + "/views/index.html")
 });
 
+
+
+app.listen(PORT);
