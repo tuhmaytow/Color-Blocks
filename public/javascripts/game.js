03c7cfbf765be2c20ef2d507c806c8bb22eec593
nica lazatin <tuhmaytow@gmail.com>
2017-05-01 14:54:13 -0700

    try responsive

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 var Game = function(myCanvas, context) {
   this.canvas = document.getElementById("myCanvas");
@@ -22,7 +22,7 @@ Game.prototype.addEntity = function (entity) {
 Game.prototype.render = function () {
   //erase the page
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  this.context.fillStyle = "#4B5267";
+  this.context.fillStyle = "#416788";
   this.context.fillRect(0,0,900,900);
   for (var i = 0; i < this.entities.length; i++) {
   	this.entities[i].draw(this.context);
@@ -33,7 +33,7 @@ Game.prototype.render = function () {
 };
 
 Game.prototype.addNewSquare = function () {
-  this.currentPiece = new Square(150, 0, this.colorOption[Math.floor(Math.random()*4)]); //"150" to have the currentPiece fall from the middle
+  this.currentPiece = new Square(225, 0, this.colorOption[Math.floor(Math.random()*4)]); //"150" to have the currentPiece fall from the middle
   this.addEntity(this.currentPiece);
 };
 
@@ -52,12 +52,12 @@ Game.prototype.goingDown = function () {
     if(this.paused){
       clearInterval(this.time);
     } else { //when its not paused
-      this.time = setInterval(this.move.bind(this, "down"), 800) //gravity of the square going down
+      this.time = setInterval(this.move.bind(this, "down"), 800); //gravity of the square going down
     }
 };
 
 Game.prototype.validateMove = function (x, y, direction) {
-  if (x < 0 || x > this.canvas.width - 25) { //if x is less than 0, or if x is less than the canvas width 
+  if (x < 0 || x > this.canvas.width - 25) { //if x is less than 0, or if x is less than the canvas width
     return false;
   } else {
     for(var i = 0; i < this.viruses.length; i++){
@@ -75,12 +75,12 @@ Game.prototype.validateMove = function (x, y, direction) {
 
 Game.prototype.move = function (direction) {
   //console.log(direction)
-  if(direction === "left"){ 
+  if(direction === "left"){
     if(this.validateMove(this.currentPiece.x - 25, this.currentPiece.y, direction)){
       this.currentPiece.x -= 25;
-    } 
+    }
   } else if(direction === "right"){
-    if(this.validateMove(this.currentPiece.x + 25, this.currentPiece.y, direction)){ 
+    if(this.validateMove(this.currentPiece.x + 25, this.currentPiece.y, direction)){
       this.currentPiece.x += 25;
     }
   } else if(direction === "down"){
@@ -88,8 +88,8 @@ Game.prototype.move = function (direction) {
       if(this.currentPiece.x === this.viruses[i].x){
         if(this.currentPiece.y === this.viruses[i].y - 25){
           if(this.currentPiece.color === this.viruses[i].color){ //if both square matches
-            this.currentPiece.color = "#4B5267"; //replace color of currentPiece to canvas color
-            this.viruses[i].color = "#4B5267";  //replace color of virus to canvas color
+            this.currentPiece.color = "#416788"; //replace color of currentPiece to canvas color
+            this.viruses[i].color = "#416788";  //replace color of virus to canvas color
             this.soundEffect();
             this.viruses.splice(i, 1);  //delete from this.viruses
             this.addNewSquare(); //add new square once virus gets deleted
@@ -101,10 +101,10 @@ Game.prototype.move = function (direction) {
             this.unmatched.push(this.currentPiece);
             // debugger
           //   if(!this.checkedUnmatched()){
-          //     this.gameOver();  
+          //     this.gameOver();
           //     return;
           // }
-            this.addNewSquare(); 
+            this.addNewSquare();
             this.addNewVirus();
           }
         // return;
@@ -114,21 +114,21 @@ Game.prototype.move = function (direction) {
     for(var i = 0; i < this.unmatched.length; i++){
       if(this.currentPiece.x === this.unmatched[i].x){
         if(this.currentPiece.y === this.unmatched[i].y - 25){
-          if(this.currentPiece.color === this.unmatched[i].color){ 
-            this.currentPiece.color = "#4B5267"; 
-            this.unmatched[i].color = "#4B5267";  
-            this.unmatched.splice(i, 1);  
-            this.addNewSquare(); 
-            this.addNewVirus(); 
+          if(this.currentPiece.color === this.unmatched[i].color){
+            this.currentPiece.color = "#416788";
+            this.unmatched[i].color = "#416788";
+            this.unmatched.splice(i, 1);
+            this.addNewSquare();
+            this.addNewVirus();
             this.points++;
             this.scoreUpdate();
           } else {
             this.unmatched.push(this.currentPiece);
             if(!this.checkedUnmatched()){
-              this.gameOver();  
+              this.gameOver();
               return;
             }
-            this.addNewSquare(); 
+            this.addNewSquare();
             this.addNewVirus();
           }
         return;
@@ -144,12 +144,12 @@ Game.prototype.move = function (direction) {
 };
 
 Game.prototype.virusCoordinates = function () { //matches the coordinates of the square and virus
-  let xCoords = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275]; 
+  let xCoords = [0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275];
   let yCoords = [125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375]; //level down the viruses
   var similar = true;
   if(this.viruses.length > 0){
-  while(similar){  
-    //console.log("Run virus coords");                                                
+  while(similar){
+    //console.log("Run virus coords");
       var coords = [];
       coords[0] = xCoords[Math.floor(Math.random() * xCoords.length)];
       coords[1] = yCoords[Math.floor(Math.random() * yCoords.length)];
@@ -182,25 +182,25 @@ Game.prototype.pauseGame = function () {
       this.paused = true;
       this.goingDown();
     }
-}
+};
 
 Game.prototype.gameOver = function () {
  this.over = true;
  clearInterval(this.time);
- alert("GAME OVER!!!")
-}
+ alert("GAME OVER!!!");
+};
 
 Game.prototype.checkedUnmatched = function () {
   var filtered = this.unmatched.filter(function(element){
     return element.y <= 0;
-  })
+  });
 return filtered.length === 0; //return true if no square overflow otherwise false
-}
+};
 
 Game.prototype.soundEffect = function () {
-  var sound = new Audio("/public/audio/drm64_mario4.mp3"); // buffers automatically when created
+  var sound = new Audio("../public/audio/drm64_mario4.mp3"); // buffers automatically when created
   sound.play();
-}
+};
 
 
 
@@ -226,8 +226,8 @@ Game.prototype.soundEffect = function () {
 // Game.prototype.virusCoordinates = function () {
 //   if(this.viruses.length > 0){
 //     var similar = true;
-//     while(similar){                                                  
-//       var x = [Math.floor(Math.random() * xCoords.length)]; // get random number between 1 and (this.canvas.width / 25) 
+//     while(similar){
+//       var x = [Math.floor(Math.random() * xCoords.length)]; // get random number between 1 and (this.canvas.width / 25)
 //       var y = [Math.floor(Math.random() * yCoords.length)];
 //         for(var i = 0; i < this.viruses.length; i++){
 //           if(x !== this.viruses[i].x && y !== this.viruses[i].y){
@@ -244,12 +244,3 @@ Game.prototype.soundEffect = function () {
 //   }
 // return [x, y];
 // };
-
-
-
-
-
-
-  
-
-
